7a18e60078164c9d6eedd39f3f6ac0f0ec5cace7
Suryansh-Dey <deysuryansh@gmail.com>
2025-04-06 12:27:17 +0530

    using gemini streaming API

--- src/gemini/ask.rs
+++ src/gemini/ask.rs
@@ -1,16 +1,19 @@
 use super::types::*;
-use awc::Client;
+use actix_web::dev::{Decompress, Payload};
+use awc::{Client, ClientResponse};
+use futures::Stream;
 use serde_json::{Value, json};
-use std::time::Duration;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+    time::Duration,
+};
 
 const API_TIMEOUT: Duration = Duration::from_secs(30);
 const BASE_URL: &str = "https://generativelanguage.googleapis.com/v1beta/models";
 
 pub struct GeminiResponse(Value);
 impl GeminiResponse {
-    fn new(value: Value) -> Self {
-        Self(value)
-    }
     pub fn get(&self) -> &Value {
         &self.0
     }
@@ -27,20 +30,51 @@ impl GeminiResponse {
         serde_json::from_str::<Value>(&unescaped_str).map_err(|_| self.get())
     }
 }
+pin_project_lite::pin_project! {
+    pub struct GeminiResponseStream{
+        #[pin]
+        response_stream:ClientResponse<Decompress<Payload>>,
+    }
+}
+impl GeminiResponseStream {
+    fn new(response_stream: ClientResponse<Decompress<Payload>>) -> Self {
+        Self { response_stream }
+    }
+}
+impl Stream for GeminiResponseStream {
+    type Item = Result<GeminiResponse, Box<dyn std::error::Error>>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = self.project();
+
+        match this.response_stream.poll_next(cx) {
+            Poll::Ready(Some(Ok(bytes))) => {
+                let text = String::from_utf8_lossy(&bytes).to_string();
+                if text == "]" {
+                    Poll::Ready(None)
+                } else {
+                    match serde_json::from_str(text[1..].trim()) {
+                        Ok(response) => Poll::Ready(Some(Ok(GeminiResponse(response)))),
+                        Err(error) => Poll::Ready(Some(Err(error.into()))),
+                    }
+                }
+            }
+            Poll::Ready(Some(Err(e))) => Poll::Ready(Some(Err(e.into()))),
+            Poll::Ready(None) => Poll::Ready(None),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
 
 pub struct Gemini<'a> {
     client: Client,
-    api_key: &'a str,
-    model: &'a str,
+    api_key: String,
+    model: String,
     sys_prompt: Option<SystemInstruction<'a>>,
     generation_config: Option<Value>,
 }
 impl<'a> Gemini<'a> {
-    pub fn new(
-        api_key: &'a str,
-        model: &'a str,
-        sys_prompt: Option<SystemInstruction<'a>>,
-    ) -> Self {
+    pub fn new(api_key: String, model: String, sys_prompt: Option<SystemInstruction<'a>>) -> Self {
         Self {
             client: Client::builder().timeout(API_TIMEOUT).finish(),
             api_key,
@@ -49,8 +83,11 @@ impl<'a> Gemini<'a> {
             generation_config: None,
         }
     }
-    pub fn set_model(&mut self, model: &'a str) {
+    pub fn set_model(&mut self, model: String) {
         self.model = model;
+    }
+    pub fn set_api_key(&mut self, api_key: String) {
+        self.api_key = api_key;
     }
     pub fn set_generation_config(&mut self, generation_config: Value) -> &mut Self {
         self.generation_config = Some(generation_config);
@@ -90,7 +127,7 @@ impl<'a> Gemini<'a> {
             .json()
             .await?;
 
-        Ok(GeminiResponse::new(response))
+        Ok(GeminiResponse(response))
     }
     pub async fn ask(
         &self,
@@ -116,6 +153,37 @@ impl<'a> Gemini<'a> {
             .json()
             .await?;
 
-        Ok(GeminiResponse::new(response))
+        Ok(GeminiResponse(response))
+    }
+    pub async fn ask_as_stream(
+        &self,
+        session: &'a mut Session,
+        question: Vec<Part>,
+    ) -> Result<GeminiResponseStream, Box<dyn std::error::Error>> {
+        let req_url = format!(
+            "{BASE_URL}/{}:streamGenerateContent?key={}",
+            self.model, self.api_key
+        );
+        let history = session.get_history_mut();
+        history.push(Chat::new(Role::user, question));
+
+        let response = self
+            .client
+            .post(req_url)
+            .send_json(&GeminiBody::new(
+                self.sys_prompt.as_ref(),
+                history.as_slice(),
+                self.generation_config.as_ref(),
+            ))
+            .await?;
+        if !response.status().is_success() {
+            return Err(format!(
+                "Found status due to {} from Gemini endpoint",
+                response.status()
+            )
+            .into());
+        }
+
+        Ok(GeminiResponseStream::new(response))
     }
 }
--- src/gemini/tests/ask.rs
+++ src/gemini/tests/ask.rs
@@ -1,12 +1,13 @@
 use crate::gemini::ask::Gemini;
-use crate::gemini::types::{Part, SystemInstruction};
+use crate::gemini::types::{Part, Session, SystemInstruction};
+use futures::StreamExt;
 use serde_json::json;
 
 #[actix_web::test]
 async fn ask_string() {
     let response = Gemini::new(
-        &std::env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY not found"),
-        "gemini-1.5-flash",
+        std::env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY not found"),
+        "gemini-1.5-flash".to_string(),
         None,
     )
     .ask_string("Hi".to_string())
@@ -18,8 +19,8 @@ async fn ask_string() {
 #[actix_web::test]
 async fn ask_string_for_json() {
     let response = Gemini::new(
-        &std::env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY not found"),
-        "gemini-1.5-flash",
+        std::env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY not found"),
+        "gemini-1.5-flash".to_string(),
         Some(SystemInstruction::new(&[Part::text("Calssify the given words".to_string())])),
     )
     .set_json_mode(json!({
@@ -40,4 +41,21 @@ async fn ask_string_for_json() {
     .await
     .unwrap();
     println!("{:?}", response.get_as_json().unwrap());
+}
+
+#[actix_web::test]
+async fn ask_streamed() {
+    let mut session = Session::new(5);
+    let ai = Gemini::new(
+        std::env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY not found"),
+        "gemini-1.5-flash".to_string(),
+        None,
+    );
+    let mut response_stream = ai
+        .ask_as_stream(&mut session, vec![Part::text("How to learn machine learning".to_string())])
+        .await
+        .unwrap();
+    while let Some(response) = response_stream.next().await {
+        println!("{}", response.unwrap().get_as_string().unwrap());
+    }
 }
